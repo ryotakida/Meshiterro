5a6401fd065fa11f4413d7c29e2cee0d63bc6929
ryotakida <uulee5521@gmail.com>
2021-09-13 07:20:27 +0000

    [finish]4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'a9242e0a45d5200ecdc365e6dc404e1db96ca7ee10f9239d5c69f72eaa5637cdc4fb76e0b5775ccf7dea9dc5a1d37130ff9d79878b0d2435119aa6d9c1acd23b'
